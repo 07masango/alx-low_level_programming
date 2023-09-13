c475664bc34a28c329dc6882bc8dc8e27635032e
07masango <thembamasangoj11@gmail.com>
2023-09-13 12:20:26 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_islower - Searches for lowercase char
+ *@c: Char to be searched
+ *Return: 1 for lowercase or 0 for anything else
+ */
+
+int _islower(int c)
+
+{
+	if (c >= 97 && c <= 122)
+	{
+	
+	return (1);
+	}
+	return (0);
+}
