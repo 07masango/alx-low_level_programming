4a0085e782cb0131ac0aa1fc528cf15edeade055
07masango <thembamasangoj11@gmail.com>
2023-09-13 14:24:09 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Add 2 values
+ * @n1: The 1st num to be figured
+ * @n2: The 2nd mun to be figured
+ * Return: int value
+ */
+
+int add(int n1, int n2)
+
+{
+
+return (n1 + n2);
+
+}
